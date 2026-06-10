9e8ceb167e8902bd50b4830565ff1a07d50ab789
ISOLCODING <rrqhoshi891@gmail.com>
2026-06-11 01:49:10 +0700

    chore: update google site verification id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -112,7 +112,7 @@ export const metadata: Metadata = {
   },
 
   verification: {
-    google: "e408fba496e16d62",
+    google: "IQ5mNNmZtFXAp7w12RrW3RX-zD-thK-yQJ9b87Wf7MM",
     yandex: "YANDEX_VERIFICATION_CODE",
     yahoo: "YAHOO_VERIFICATION_CODE",
     other: {
@@ -156,7 +156,7 @@ export const metadata: Metadata = {
   },
 
   other: {
-    "google-site-verification": "e408fba496e16d62",
+    "google-site-verification": "IQ5mNNmZtFXAp7w12RrW3RX-zD-thK-yQJ9b87Wf7MM",
     "msvalidate.01": "BING_CODE",
     "p:domain_verify": "PINTEREST_CODE",
     "fb:app_id": "FACEBOOK_APP_ID",
